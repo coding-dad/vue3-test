b14120f8b75cfab6489c98b32b8f1f02528a5c31
coding-dad <boris.365@outlook.com>
2020-05-10 11:19:18 +0200

    innitial commit

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,14 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
 
-export default Vue.extend({
+export default {
   name: 'App',
   components: {
     HelloWorld
   }
-});
+};
 </script>
 
 <style lang="scss">
